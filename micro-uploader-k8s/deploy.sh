eafb30e84de30882734ecc5dd6cb0a3536ecb605
Manuelo Salumbides II <96850774+manuelosalumbidesii@users.noreply.github.com>
2025-07-25 21:02:22 +0800

    Update deploy.sh

--- micro-uploader-k8s/deploy.sh
+++ micro-uploader-k8s/deploy.sh
@@ -4,14 +4,13 @@ echo "🚀 Deploying Image Uploader to Minikube..."
 
 cd "$(dirname "$0")" || exit
 
-# Step 1: Apply all Kubernetes manifests
+
 kubectl apply -f k8s/namespace.yml
 kubectl apply -f k8s/pvc.yml
 kubectl apply -f k8s/backend.yml
 kubectl apply -f k8s/frontend.yml
 kubectl apply -f k8s/ingress.yml
 
-# Step 2: Show deployment status
 echo
 echo "✅ Deployment complete. Current status:"
 kubectl get all -n image-uploader
